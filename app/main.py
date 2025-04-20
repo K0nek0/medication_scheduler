9b92d8339d98153828948bc206a4339324c931b6
Timoshchenko Nikolay <silent_master@mail.ru>
2025-04-20 22:15:35 +0200

    add router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
-from fastapi import FastAPI, HTTPException, Query
-from services import create_schedule, get_schedules, get_schedule_detail, calculate_schedule_times, get_takings
-from models import MedicationSchedule, ScheduleCreate, ScheduleResponse, ScheduleDetail, NextTakings
-from typing import List
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import db
-from datetime import time
-from utils import generate_schedule_id
+from rest import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,42 +10,4 @@ async def lifespan(app: FastAPI):
     await db.pool.close()
 
 app = FastAPI(lifespan=lifespan)
-
-@app.post("/schedule", response_model=ScheduleResponse)
-async def create_new_schedule(schedule: ScheduleCreate):
-    new_schedule = MedicationSchedule(
-        schedule_id = generate_schedule_id(user_id=schedule.user_id),
-        user_id=schedule.user_id,
-        name=schedule.name,
-        frequency=schedule.frequency,
-        duration=schedule.duration,
-        start_time = time(8, 0),
-        end_time = time(22, 0)
-    )
-    await create_schedule(new_schedule)
-    return {"schedule_id": new_schedule.schedule_id}
-
-@app.get("/schedules", response_model=List[int])
-async def get_user_schedules(user_id: int = Query(..., description="ID пользователя")):
-    return await get_schedules(user_id)
-
-@app.get("/schedule", response_model=ScheduleDetail)
-async def get_schedule(
-    user_id: int = Query(..., description="ID пользователя"),
-    schedule_id: int = Query(..., description="ID расписания")):
-
-    detail = await get_schedule_detail(user_id, schedule_id)
-    if not detail:
-        raise HTTPException(status_code=404, detail="Расписание не найдено")
-
-    times = calculate_schedule_times(detail['frequency'], detail['start_time'], detail['end_time'])
-    return {
-        "name": detail['name'],
-        "frequency": detail['frequency'],
-        "duration": detail['duration'],
-        "schedule": times
-    }
-
-@app.get("/next_takings", response_model=List[NextTakings])
-async def get_next_takings(user_id: int = Query(..., description="ID пользователя")):
-    return await get_takings(user_id)
+app.include_router(router)
---
+++ app/rest.py
@@ -0,0 +1,47 @@
+from fastapi import HTTPException, Query, APIRouter
+from services import create_schedule, get_schedules, get_schedule_detail, calculate_schedule_times, get_takings
+from models import MedicationSchedule, ScheduleCreate, ScheduleResponse, ScheduleDetail, NextTakings
+from typing import List
+from datetime import time
+from utils import generate_schedule_id
+
+router = APIRouter(prefix="/api/v1")
+
+@router.post("/schedule", response_model=ScheduleResponse)
+async def create_new_schedule(schedule: ScheduleCreate):
+    new_schedule = MedicationSchedule(
+        schedule_id = generate_schedule_id(user_id=schedule.user_id),
+        user_id=schedule.user_id,
+        name=schedule.name,
+        frequency=schedule.frequency,
+        duration=schedule.duration,
+        start_time = time(8, 0),
+        end_time = time(22, 0)
+    )
+    await create_schedule(new_schedule)
+    return {"schedule_id": new_schedule.schedule_id}
+
+@router.get("/schedules", response_model=List[int])
+async def get_user_schedules(user_id: int = Query(..., description="ID пользователя")):
+    return await get_schedules(user_id)
+
+@router.get("/schedule", response_model=ScheduleDetail)
+async def get_schedule(
+    user_id: int = Query(..., description="ID пользователя"),
+    schedule_id: int = Query(..., description="ID расписания")):
+
+    detail = await get_schedule_detail(user_id, schedule_id)
+    if not detail:
+        raise HTTPException(status_code=404, detail="Расписание не найдено")
+
+    times = calculate_schedule_times(detail['frequency'], detail['start_time'], detail['end_time'])
+    return {
+        "name": detail['name'],
+        "frequency": detail['frequency'],
+        "duration": detail['duration'],
+        "schedule": times
+    }
+
+@router.get("/next_takings", response_model=List[NextTakings])
+async def get_next_takings(user_id: int = Query(..., description="ID пользователя")):
+    return await get_takings(user_id)
