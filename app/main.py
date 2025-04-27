f091c75bc1950b2d17772d37359c37e82645456f
Timoshchenko Nikolay <silent_master@mail.ru>
2025-04-27 23:59:48 +0200

    fixing

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from db.database import db
 from services.rest_service import router
-import threading
 from services.grpc_service import serve
 import asyncio
 
@@ -10,10 +9,16 @@ import asyncio
 async def lifespan(app: FastAPI):
     await db.create_pool()
 
-    grpc_thread = threading.Thread(target=asyncio.run, args=(serve(),), daemon=True)
-    grpc_thread.start()
+    grpc_task = asyncio.create_task(serve())
 
     yield
+
+    grpc_task.cancel()
+    try:
+        await grpc_task
+    except asyncio.CancelledError:
+        pass
+
     await db.pool.close()
 
 app = FastAPI(lifespan=lifespan)
