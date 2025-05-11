bc3460ed7b18f47142d0e620de891844d01ec308
Timoshchenko Nikolay <silent_master@mail.ru>
2025-05-12 01:53:22 +0200

    fix

--- Justfile
+++ Justfile
@@ -1,10 +1,77 @@
 set shell := ["powershell.exe"]
 
-test:
-    pytest tests/rest
+# # Directories for source code and tests
+# SOURCE_DIR := "app"
+# TESTS_DIR := "tests"
 
+# Default command: list available commands
+default:
+    @just --list
+
+# # Environment variables file
+# set dotenv-filename := ".env"
+
+# # Run all checks: linters and formatting validation
+# lint: ruff-check mypy format-check
+
+# --- Dependency Management ---
+
+# Update project dependencies
+[group('dependencies')]
+update:
+    uv pip install --upgrade -r requirements.txt
+
+# # --- Linters and Formatting ---
+
+# # Automatically format code
+# [group('linters')]
+# format:
+#     uv run ruff format .
+
+# # Check formatting without modifying files
+# [group('linters')]
+# format-check:
+#     uv run ruff format .
+
+# # Lint code using Ruff
+# [group('linters')]
+# ruff-check:
+#     ruff check {{ SOURCE_DIR }}
+#     ruff check {{ TESTS_DIR }}
+
+# # Fix code using Ruff
+# [group('linters')]
+# ruff:
+#     ruff check --fix --unsafe-fixes {{ SOURCE_DIR }}
+#     ruff check --fix --unsafe-fixes {{ TESTS_DIR }}
+
+# # Type checking using mypy
+# [group('linters')]
+# mypy:
+#     uv run mypy .
+
+# --- Testing ---
+
+# # Run tests using pytest
+# [group('testing')]
+# tests:
+#     uv run pytest tests/rest
+
+# # --- Building ---
+
+# # Build DataBase
+# [group('building')]
+# build-depends:
+#     docker compose -f .docker/docker-compose-local.yml up -d --remove-orphans --build
+
+# Generate pydantic models
 generate-models:
     datamodel-codegen --input ./schemas/openapi.yaml --output ./app/models/models_auto.py --force
 
+# Generate protoc
 generate-grpc:
-    python -m grpc_tools.protoc -Iapp/protos --python_out=app/grpc_package --grpc_python_out=app/grpc_package schemas/med_schedule.proto
+    python -m grpc_tools.protoc -Ischemas --python_out=app/grpc_package --grpc_python_out=app/grpc_package schemas/med_schedule.proto
+
+# Run app
+run:
+    python -m app
--- app/services/grpc_service.py
+++ app/services/grpc_service.py
@@ -8,31 +8,20 @@ from app.grpc_package.med_schedule_pb2 import (
 )
 from app.core.handler import create_schedule, get_schedules, get_schedule_detail, get_takings
 from app.core.utils import calculate_schedule_times
-from app.models.med_schedule_model import MedicationSchedule
-from datetime import time
-from app.core.utils import generate_schedule_id
-from app.db.database import db
+from app.models.models_auto import ScheduleCreate
 # from logger.business_logger import business_logger
 
 class MedicationScheduleServicer(med_schedule_pb2_grpc.MedicationScheduleServiceServicer):
     # @business_logger("grpc_create_schedule")
     async def CreateSchedule(self, request, context):
-        new_schedule = MedicationSchedule(
-            schedule_id=generate_schedule_id(user_id=request.user_id),
+        schedule_data = ScheduleCreate(
             user_id=request.user_id,
             name=request.name,
             frequency=request.frequency,
-            duration=request.duration,
-            start_time=time(8, 0),
-            end_time=time(22, 0)
+            duration=request.duration
         )
-        await create_schedule(new_schedule)
-        return ScheduleResponse(schedule_id=new_schedule.schedule_id)
-
-    # @business_logger("grpc_get_user_schedules")
-    async def GetUserSchedules(self, request, context):
-        schedule_ids = await get_schedules(request.user_id)
-        return UserSchedulesResponse(schedule_ids=schedule_ids)
+        schedule_id = await create_schedule(schedule_data)
+        return ScheduleResponse(schedule_id=str(schedule_id))
 
     # @business_logger("grpc_get_schedule_detail")
     async def GetScheduleDetail(self, request, context):
@@ -48,6 +37,11 @@ class MedicationScheduleServicer(med_schedule_pb2_grpc.MedicationScheduleService
             duration=detail['duration'],
             schedule=times
         )
+
+    # @business_logger("grpc_get_user_schedules")
+    async def GetUserSchedules(self, request, context):
+        schedule_ids = await get_schedules(request.user_id)
+        return UserSchedulesResponse(schedule_ids=schedule_ids)
 
     # @business_logger("grpc_get_next_takings")
     async def GetNextTakings(self, request, context):
